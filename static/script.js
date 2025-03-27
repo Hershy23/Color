c7a0118f10e7f449e36b1b8027785eee3022df47
Hershy23 <harshitapatnaik2304@gmail.com>
2025-03-27 18:45:11 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -24,11 +24,16 @@ document.getElementById("predictBtn").addEventListener("click", function () {
     const formData = new FormData();
     formData.append("file", file);
 
-    fetch("]https://color-2-5pxn.onrender.com/predict", {  // Update this URL
-    method: "POST",
-    body: formData
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error("Error:", error));
-
+    fetch("https://color-2-5pxn.onrender.com/predict", { // Fixed URL
+        method: "POST",
+        body: formData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error! Status: " + response.status);
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
+});
